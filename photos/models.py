60adf9c45a028335f0b2310b5753a2dd3ab0a216
SkinnyFatSumo <simon.coffin.website@gmail.com>
2019-07-18 15:47:49 -0700

    removed erroneous call to lower() on CustomUser object

--- photos/models.py
+++ photos/models.py
@@ -30,7 +30,7 @@ class Tag(models.Model):
 def upload_photo_to(instance, filename):
     filename_base, filename_ext = os.path.splitext(filename)
     return 'photos/%s/%s/%s/%s/' % (
-        now().strftime("%Y%m%d"), instance.owner.lower(), instance.title.lower(), filename_ext.lower(),
+        now().strftime("%Y%m%d"), instance.owner, instance.title.lower(), filename_ext.lower(),
     )
 
 # Create PHOTO (that can be associated with tags)
