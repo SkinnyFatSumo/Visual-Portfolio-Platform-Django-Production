6b55465df424a3184f7da2a9829caa7a6c3cc8b9
SkinnyFatSumo <simon.coffin.website@gmail.com>
2019-07-25 22:42:35 -0700

    added capability for handling orientation issues on mobile uploads when converted to thumbnails, keeping them in proper orientation

--- photos/models.py
+++ photos/models.py
@@ -100,7 +100,27 @@ class Photo(models.Model):
         except:
             raise Exception('Failed to open full res image')
             return False
-       
+     
+        # Handle orientation exif data if found
+        try:
+            if hasattr(image, '_getexif'): # only present in JPEGs
+                for orientation in ExifTags.TAGS.keys(): 
+                    if ExifTags.TAGS[orientation]=='Orientation':
+                        break 
+                e = image._getexif()       # returns None if no EXIF data
+                if e is not None:
+                    exif=dict(e.items())
+                    orientation = exif[orientation] 
+
+                    if orientation == 3:   image = image.transpose(Image.ROTATE_180)
+                    elif orientation == 6: image = image.transpose(Image.ROTATE_270)
+                    elif orientation == 8: image = image.transpose(Image.ROTATE_90)
+
+        except:
+            raise Exception('Failure while checking exif data.')
+            return False
+        
+
         # resize image to max 700px on longest side
         size = (700, 700)
         image.thumbnail(size, Image.ANTIALIAS)
